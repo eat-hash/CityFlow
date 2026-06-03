bfcf74bc77b89b78dc7f1fd366014bf22aaa1efb
吃哈希 <2184469378@qq.com>
2026-06-03 20:08:34 +0800

    Revert "Merge pull request #9 from eat-hash/feature/后端-数据库设计"
    This reverts commit 57a2bb51a3d14fb9faf1f90f5e8f553a487d578e, reversing
    changes made to aac0d9844795e45c156b1c3431fd130818b955c0.

--- sql/init_data.sql
+++
@@ -1,7 +0,0 @@
-USE traffic_db;
-
-INSERT INTO intersection (intersection_id, name, code, region, lane_count)
-VALUES 
-('I001', '解放大道-建设大道', 'X001', '江汉区', 6),
-('I002', '中山大道-友谊路', 'X002', '江岸区', 4),
-('I003', '光谷大道-高新大道', 'X003', '洪山区', 8);
