359ee7e64b87b393731f322e573e7b9de7168720
shuii1129 <93443449@qq.com>
2026-06-17 17:17:54 +0800

    补全entity实体类，同步完整后端B数据库层代码

---
+++ web/backend/src/entity/Intersection.java
@@ -0,0 +1,20 @@
+package backend.src.entity;
+
+public class Intersection {
+    private String intersectionId;
+    private String name;
+    private String code;
+    private String region;
+    private int laneCount;
+
+    public String getIntersectionId() { return intersectionId; }
+    public void setIntersectionId(String intersectionId) { this.intersectionId = intersectionId; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
+    public String getRegion() { return region; }
+    public void setRegion(String region) { this.region = region; }
+    public int getLaneCount() { return laneCount; }
+    public void setLaneCount(int laneCount) { this.laneCount = laneCount; }
+}
